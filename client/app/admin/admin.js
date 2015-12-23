5300fdd9246cfa2b953ec2354386d0ed5a44fa5e
unknown <arupa.datta@NAP-LT140806AW7.multiplan.com>
2015-12-23 11:20:22 -0600

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bengaleerasanaApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
